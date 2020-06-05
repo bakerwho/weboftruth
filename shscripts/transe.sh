faeb145be43038a4624becd2563906daf39280fb
bakerwho <aabir22@gmail.com>
2020-06-05 14:06:01 -0400

    GPU time woo

--- shscripts/transe.sh
+++ shscripts/transe.sh
@@ -2,11 +2,13 @@
 #SBATCH --job-name=aabir-transe
 #SBATCH --output=/project2/jevans/aabir/weboftruth/logs/transe-full.out
 #SBATCH --error=/project2/jevans/aabir/weboftruth/logs/transe-full.err
-#SBATCH --partition=broadwl
 #SBATCH --mem=31GB
 #SBATCH --time=32:00:00
+#SBATCH --partition=gpu2
+#SBATCH --gres=gpu:N
 
 module load Anaconda3/5.3.0
+module load cuda/9.1
 
 echo 'run started at ' $(date)
 python /project2/jevans/aabir/weboftruth/pyscripts/train_save_model.py -e 200 -m 'TransE'
