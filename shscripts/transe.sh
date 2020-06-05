80886703c809b17a99b1c9918e7d60cb7de0c0e3
bakerwho <aabir22@gmail.com>
2020-06-05 12:56:20 -0400

    pls:

--- shscripts/transe.sh
+++ shscripts/transe.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name=aabir-transe
-#SBATCH --output=/project2/jevans/aabir/weboftruth/logs/transe-2.out
-#SBATCH --error=/project2/jevans/aabir/weboftruth/logs/transe-2.err
+#SBATCH --output=/project2/jevans/aabir/weboftruth/logs/transe-full.out
+#SBATCH --error=/project2/jevans/aabir/weboftruth/logs/transe-full.err
 #SBATCH --partition=broadwl
 #SBATCH --mem=31GB
 #SBATCH --time=32:00:00
