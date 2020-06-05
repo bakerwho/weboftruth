d1b13ac4b7c36d6e8c0d957adb8d760413d55a86
bakerwho <aabir@midway2-login2.rcc.local>
2020-06-05 13:11:55 -0500

    pls

--- shscripts/transe.sh
+++ shscripts/transe.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem=31GB
 #SBATCH --time=32:00:00
 #SBATCH --partition=gpu2
-#SBATCH --gres=gpu:N
+#SBATCH --gres=gpu:1
 
 module load Anaconda3/5.3.0
 module load cuda/9.1
 
 echo 'run started at ' $(date)
-python /project2/jevans/aabir/weboftruth/pyscripts/train_save_model.py -e 200 -m 'TransE'
+python /project2/jevans/aabir/weboftruth/pyscripts/train_save_model.py -e 200 -m 'TransE' -s True
 echo echo 'run ended at ' $(date)
