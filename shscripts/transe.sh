c0a1e5734e9ed54a599d3a78fb8ff38e5d7094c5
bakerwho <aabir@midway2-login2.rcc.local>
2020-06-05 22:48:09 -0500

    logs

--- shscripts/transe.sh
+++ shscripts/transe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=transe-gpu-small
+#SBATCH --job-name=gpu-sm
 #SBATCH --output=/project2/jevans/aabir/weboftruth/logs/transe-sm-gpu.out
 #SBATCH --error=/project2/jevans/aabir/weboftruth/logs/transe-sm-gpu.err
 #SBATCH --mem=31GB
@@ -11,5 +11,5 @@ module load Anaconda3/5.3.0
 module load cuda/9.1
 
 echo 'run started at ' $(date)
-python /project2/jevans/aabir/weboftruth/pyscripts/train_save_model.py -e 200 -m 'TransE' -s True
+python /project2/jevans/aabir/weboftruth/pyscripts/train_save_model.py -e 1000 -m 'TransE' -s True
 echo echo 'run ended at ' $(date)
