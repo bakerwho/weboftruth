133bd32340f981d7281244d4d2949680b69feb6e
bakerwho <aabir22@gmail.com>
2020-06-10 01:59:25 -0400

    fix to df_to_kg

--- shscripts/runall.py
+++ shscripts/runall.py
@@ -1,9 +1,11 @@
 import weboftruth as wot
 
 for ts in [100, 80, 50]:
+    print(f'running for configuration {ts}')
     tr_fn, val_fn, test_fn = wot.utils.get_file_names(ts)
     tr_df, val_df, test_df = wot.utils.read_data(tr_fn, val_fn, test_fn,
                                 wot.svo_paths[ts])
+    print(f"{[tr_df.shape, val_df.shape, test_df.shape]}")
     if ts != 100:
         tr_kg, val_kg, test_kg = (wot.utils.df_to_kg(df.drop(
             columns=['true_positive'])) for df in (tr_df, val_df, test_df))
