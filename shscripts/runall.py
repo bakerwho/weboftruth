782abb6bd915aff2c655fcb851731973d176b0d3
bakerwho <aabir22@gmail.com>
2020-06-10 01:54:52 -0400

    fix to df_to_kg

--- shscripts/runall.py
+++ shscripts/runall.py
@@ -4,7 +4,11 @@ for ts in [100, 80, 50]:
     tr_fn, val_fn, test_fn = wot.utils.get_file_names(ts)
     tr_df, val_df, test_df = wot.utils.read_data(tr_fn, val_fn, test_fn,
                                 wot.svo_paths[ts])
-    tr_kg, val_kg, test_kg = (wot.utils.df_to_kg(df.drop(columns=['true_positive'])
+    if ts != 100:
+        tr_kg, val_kg, test_kg = (wot.utils.df_to_kg(df.drop(
+            columns=['true_positive']))) for df in (tr_df, val_df, test_df))
+    else:
+        tr_kg, val_kg, test_kg = (wot.utils.df_to_kg(df)
                                 ) for df in (tr_df, val_df, test_df))
     for model_type in ['DistMult', 'HolE', 'TransE']:
         if model_type == 'TransE':
