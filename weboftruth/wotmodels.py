7b0ca957dc8fa64d97ee1b38d988a14738abdd97
Aabir <aabir22@gmail.com>
2021-03-16 17:25:38 -0500

    massive change - deleted CustomBilinearModel due to code redundancy

--- weboftruth/wotmodels.py
+++ weboftruth/wotmodels.py
@@ -81,7 +81,7 @@ class CustomTransModel():
 
     Use load_WOTmodel(model_folder) after initialization to load from disc
     """
-    def __init__(self, trainkg, traints, model_type, **kwargs):
+    def __init__(self, trainkg, traints, model_type, bilinear=False, **kwargs):
         # add train_ts/truth_share parameter
         self.trainkg = trainkg
         self.traints = traints
@@ -103,6 +103,11 @@ class CustomTransModel():
                                     n_entities=self.trainkg.n_ent,
                                     n_relations=self.trainkg.n_rel,
                                     dissimilarity_type=self.diss_type)
+            elif bilinear:
+                self.model = getattr(torchkge.models.bilinear, self.model_type + 'Model'
+                                    )(emb_dim=self.emb_dim,
+                                        n_entities = self.trainkg.n_ent,
+                                        n_relations = self.trainkg.n_rel)
             else:
                 self.model = getattr(torchkge.models, f'{model_type}Model'
                                 )(emb_dim=self.emb_dim,
@@ -112,7 +117,7 @@ class CustomTransModel():
         self.n_relations = self.trainkg.n_rel
 
         self.setup_model_folder()
-        self.save_kg(self.trainkg)
+        self.save_kg(self.trainkg, addtxt='train')
 
         self.logfile = join(self.model_path, 'log.txt')
         ## Hyperparameters
@@ -204,27 +209,29 @@ class CustomTransModel():
                 self.logline('\t\tTest loss: {np.mean(losses)}')
         return np.mean(losses)
 
-    def train_model(self, n_epochs, val_kg, ts=''):
+    def train_model(self, n_epochs, val_kg):
         epochs = tqdm(range(n_epochs), unit='epoch')
+        if self.epochs == 0:
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' UTC'
+            self.logline(f'Training started at {dt}\n')
         for epoch in epochs:
             mean_epoch_loss = self.one_epoch()
             self.logline(f'Epoch {self.epochs} | Train loss: {mean_epoch_loss}')
             if ((epoch+1)%args.ve)==0 or epoch==0:
-                self.save_model(ts=ts)
+                self.save_model()
                 val_loss = self.validate(val_kg)
                 if not self.val_losses or val_loss < min(self.val_losses):
-                    self.save_model(best=True, ts=ts)
+                    self.save_model(best=True)
                 self.logline(f'\tEpoch {self.epochs} | Validation loss: {val_loss}')
                 self.val_losses.append(val_loss)
                 self.val_epochs.append(self.epochs)
         self.logline(f"\nbest epoch: {self.best_epoch}\n")
         self.model.normalize_parameters()
 
-    def save_model(self, best=False, ts=''):
+    def save_model(self, best=False):
         # files written to self.model_path
         # save torch model
-        ts = f"_ts={ts}" if not ts else ts
-        modelname = f'e={self.epochs}{ts}_{self.model_type}_model'
+        modelname = f'e={self.epochs}_ts={self.traints}_{self.model_type}_model'
         if best:
             self.best_epoch = self.epochs
             modelname = 'best_'+modelname
@@ -232,148 +239,12 @@ class CustomTransModel():
         print(f' saving {self.model_type} to {modelpath}')
         torch.save(self.model.state_dict(), modelpath)
 
-    def save_kg(self, kg):
+    def save_kg(self, kg, addtxt=''):
         # save knowledge Graph
         if kg is None:
             kg = self.trainkg
         df = utils.kg_to_df(kg)
-        kgdfname = f'{self.model_type}_kg.csv'
-        kgdfpath = join(self.model_path, kgdfname)
-        if not os.path.exists(kgdfpath):
-            df.to_csv(kgdfpath, index=False)
-        print(f' saving trainkg to {kgdfpath}')
-
-    def load_WOTmodel(self, model_path, which='best_'):
-        self.model = utils.load_model(model_path, which)
-        self.kg = utils.load_kg(model_path)
-
-
-class CustomBilinearModel():
-    def __init__(self, kg, model_type, **kwargs):
-        self.trainkg = kg
-        self.emb_dim = kwargs.pop('emb_dim', 250)
-        self.model_type = model_type
-        self.model = getattr(torchkge.models.bilinear, self.model_type + 'Model'
-                            )(emb_dim=self.emb_dim,
-                                n_entities = self.trainkg.n_ent,
-                                n_relations = self.trainkg.n_rel)
-        all_is = [int(d.split('_')[1]) for d in os.listdir(models_path
-                                ) if os.path.isdir(join(models_path, d))
-                                    and f'{self.model_type}_' in d]
-        i = [x for x in range(1, len(all_is)+2) if x not in all_is][0]
-        self.model_path = join(models_path, f'{self.model_type}_{str(i+1).zfill(2)}')
-        os.makedirs(self.model_path, exist_ok=True)
-        self.logfile = join(self.model_path, 'log.txt')
-
-        ## Hyperparameters
-        self.lr = kwargs.pop('lr', 0.0004)
-        self.n_epochs = kwargs.pop('n_epochs', 100)
-        self.b_size = kwargs.pop('b_size', 32)
-        self.logline(tabulate([(k,v) for k, v in vars(self).items()],
-                                    headers=['variable', 'value']))
-
-        try:
-            self.dataloader = DataLoader(self.trainkg, batch_size=self.b_size, use_cuda='all')
-        except AssertionError:
-            self.dataloader = DataLoader(self.trainkg, batch_size=self.b_size)
-
-        ## Logger
-        self.epochs=0
-        self.tr_losses=[]
-        self.best_epoch=-1
-        self.val_losses=[]
-        self.val_epochs=[]
-
-    def logline(self, line):
-        with open(self.logfile, 'a+') as f:
-            f.write(line)
-            f.write('\n')
-
-    def set_optimizer(self, optClass=Adam, **kwargs):
-        self.optimizer = optClass(self.model.parameters(), lr=self.lr,
-                                    **kwargs)
-
-    def set_sampler(self, samplerClass=BernoulliNegativeSampler, **kwargs):
-        self.sampler = samplerClass(**kwargs)
-
-    def set_loss(self, lossClass=MarginLoss, **kwargs):
-        self.loss_fn = lossClass(**kwargs)
-        try:
-            self.loss_fn.cuda()
-        except:
-            pass
-
-    def one_epoch(self):
-        running_loss = 0.0
-        for i, batch in enumerate(self.dataloader):
-            h, t, r = batch
-            n_h, n_t = self.sampler.corrupt_batch(h, t, r)
-            self.optimizer.zero_grad()
-            pos, neg = self.model(h, t, n_h, n_t, r)
-            loss = self.loss_fn(pos, neg)
-            loss.backward()
-            self.optimizer.step()
-            running_loss += loss.item()
-        self.model.normalize_parameters()
-        self.epochs += 1
-        epoch_loss = running_loss/i
-        self.tr_losses.append(epoch_loss)
-        return epoch_loss
-
-    def validate(self, val_kg, istest=False):
-        losses = []
-        try:
-            dataloader = DataLoader(val_kg, batch_size=self.b_size, use_cuda='all')
-        except AssertionError:
-            dataloader = DataLoader(val_kg, batch_size=self.b_size)
-
-        for batch in dataloader:
-            h, t, r = batch
-            n_h, n_t = self.sampler.corrupt_batch(h, t, r)
-            pos, neg = self.model(h, t, n_h, n_t, r)
-            loss = self.loss_fn(pos, neg)
-            losses.append(loss.item())
-        if istest:
-            self.logline('\t\tTest loss: {np.mean(losses)}')
-        return np.mean(losses)
-
-    def train_model(self, n_epochs, val_kg, ts=''):
-        epochs = tqdm(range(n_epochs), unit='epoch')
-        for epoch in epochs:
-            if self.epochs == 0:
-                dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")+' UTC'
-                self.logline(f'Training started at {dt}\n')
-            mean_epoch_loss = self.one_epoch()
-            self.logline(f'Epoch {self.epochs} | Train loss: {mean_epoch_loss}')
-            if ((epoch+1)%args.ve)==0 or epoch==0:
-                self.save_model(ts=ts)
-                val_loss = self.validate(val_kg)
-                if not self.val_losses or val_loss < min(self.val_losses):
-                    self.save_model(best=True, ts=ts)
-                self.logline(f'\tEpoch {self.epochs} | Validation loss: {val_loss}')
-                self.val_losses.append(val_loss)
-                self.val_epochs.append(self.epochs)
-        self.logline(f"\nbest epoch: {self.best_epoch}\n")
-        self.model.normalize_parameters()
-
-    def save_model(self, best=False, ts=''):
-        # files written to self.model_path
-        # save torch model
-        ts = f"_ts={ts}" if not ts else ts
-        modelname = f'e={self.epochs}{ts}_{self.model_type}_model'
-        if best:
-            self.best_epoch = self.epochs
-            modelname = 'best_'+modelname
-        modelpath = join(self.model_path, modelname+'.pt')
-        print(f' saving {self.model_type} to {modelpath}')
-        torch.save(self.model.state_dict(), modelpath)
-
-    def save_kg(self, kg):
-        # save knowledge Graph
-        if kg is None:
-            kg = self.trainkg
-        df = utils.kg_to_df(kg)
-        kgdfname = f'{self.model_type}_kg.csv'
+        kgdfname = f'{addtxt}_{self.model_type}_kg.csv'
         kgdfpath = join(self.model_path, kgdfname)
         if not os.path.exists(kgdfpath):
             df.to_csv(kgdfpath, index=False)
@@ -430,4 +301,7 @@ if __name__ == '__main__':
         mod.model.cuda()
         mod.loss_fn.cuda()
     mod.train_model(args.epochs, tr_kg)
+    mod.save_kg(test_kg, 'test')
     mod.validate(test_kg, istest=True)
+    #mod.save_kg(val_kg, 'val')
+    #mod.validate(val_kg, istest=True)
