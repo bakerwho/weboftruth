011314095aec40a5244d991f8642e52a4b577c10
Aabir <aabir22@gmail.com>
2021-06-01 15:37:51 -0500

    minor bugfix

--- weboftruth/utils.py
+++ weboftruth/utils.py
@@ -45,15 +45,15 @@ def read_data(tr_fn, val_fn, test_fn, path, explode_rels=False, rel_sep=None,
         if rel_sep is None:
             print("Using default separator: '/'")
             rel_sep = '/'
-        tr_df, val_df, test_df = (explode_rel_column(df, colnames[1], rel_sep)
-                                    for df in (tr_df, val_df, test_df))
+        tr_df, val_df, test_df = [explode_rel_column(df, colnames[1], rel_sep)
+                                    for df in (tr_df, val_df, test_df)]
 
     return tr_df, val_df, test_df
 
 def explode_rel_column(df, rel_colname='rel', rel_sep='/'):
     assert rel_colname in df.columns, f"'{rel_colname}' not in column names"
     rel_col2 = rel_colname+'_full'
-    df.rename(columns={rel_colname: rel_col2, inplace=True)
+    df.rename(columns={rel_colname: rel_col2}, inplace=True)
     df[rel_colname] = df.rel2.apply(lambda x: [i for i in x.split(
                                 rel_sep) if len(i)>0])
     df = df.explode(rel_colname)
