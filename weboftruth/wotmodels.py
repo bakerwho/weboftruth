d96c9618b101fe93eb3fb5985a4071384b9c31ad
Aabir <aabir22@gmail.com>
2021-06-02 02:15:19 -0500

    fixes

--- weboftruth/wotmodels.py
+++ weboftruth/wotmodels.py
@@ -287,7 +287,7 @@ if __name__ == '__main__':
 
     # Load data
     #tr_fn, val_fn, test_fn = wot.utils.get_svo_file_names(args.ts)
-    as_id = 'FB15K' in args.dataset
+    as_id = 'FB15K' not in args.dataset
     tr_fn, val_fn, test_fn = wot.utils.get_simonepri_filenames(args.datapath,
                                                             args.dataset,
                                                             id=as_id)
